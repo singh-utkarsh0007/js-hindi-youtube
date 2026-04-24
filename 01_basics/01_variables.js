5147743980b7f2fa8b2a044619ec1aac2b6f07b2
singh-utkarsh0007 <utkarshsingh739834@gmail.com>
2026-04-24 16:55:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =192883
+let accountEmail ="ram@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2 // not allowed
+
+accountEmail = "reena@12.com"
+accountPassword = "493339"
+accountCity = "Lucknow"
+
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+//console.table([accountId,accountEmail,accountPassword,accountCity])
