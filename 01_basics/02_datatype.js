b5db546430ae8356a9fe59c34d2107a2f750be76
singh-utkarsh0007 <utkarshsingh739834@gmail.com>
2026-04-24 16:54:38 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+console.log(typeof hindu);
+console.log(typeof Utkarsh);
+console.log(typeof age);
+console.log(typeof Utkarsh)
+console.log(typeof null)
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol 
+
+// object
